662ae4773d34d284cb06d5056e25a97800555efb
Jacques Benzakein <Jackbenzakein@gmail.com>
2020-04-07 18:37:00 +0300

    Added a custom button class called GFButton and set up a few configuration methods including an initializer taking in a background color and title

---
+++ GHFollowers/GFButton.swift
@@ -0,0 +1,36 @@
+//
+//  GFButton.swift
+//  GHFollowers
+//
+//  Created by Jacques Benzakein on 4/7/20.
+//  Copyright © 2020 Q Technologies. All rights reserved.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(withBackgroundColor backgronudColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
