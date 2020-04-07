c23a90bbfa3ac52e1da3eab17694e0a5121089d8
Jacques Benzakein <Jackbenzakein@gmail.com>
2020-04-07 18:42:09 +0300

    Added skeleton of custom text field class

---
+++ GHFollowers/GFTextField.swift
@@ -0,0 +1,13 @@
+//
+//  GFTextField.swift
+//  GHFollowers
+//
+//  Created by Jacques Benzakein on 4/7/20.
+//  Copyright © 2020 Q Technologies. All rights reserved.
+//
+
+import UIKit
+
+class GFTextField: UITextField { 
+
+}
