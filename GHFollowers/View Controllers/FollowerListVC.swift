3e8007a97703284e6deafc121ab5c946da4a77ac
Jacques Benzakein <Jackbenzakein@gmail.com>
2020-04-21 16:09:28 +0300

    Modified the FollowerListVC to work with the updated implementation of getFollowers that now uses the Result type

--- GHFollowers/View Controllers/FollowerListVC.swift
+++ GHFollowers/View Controllers/FollowerListVC.swift
@@ -18,14 +18,22 @@ class FollowerListVC: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        NetworkManager.shared.getFollowers(for: username, page: 1) { (followers, errorMessage) in
-            guard let followers = followers else {
-                self.presentGFAlertOnMainThread(title: "Uh oh.", message: errorMessage!, buttonTitle: "Got it.")
-                return
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            
+            switch result {
+            case .success(let followers):
+                print(followers.count)
+                print(followers)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Uh oh.", message: error.rawValue, buttonTitle: "Got it.")
             }
             
-            print(followers.count)
-            print(followers)
+//            guard let followers = followers else {
+//                self.presentGFAlertOnMainThread(title: "Uh oh.", message: errorMessage!, buttonTitle: "Got it.")
+//                return
+//            }
+//
+            
         }
     }
     
