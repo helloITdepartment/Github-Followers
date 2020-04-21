964b122df0861cf2136055bd803e37e82c549a8d
Jacques Benzakein <Jackbenzakein@gmail.com>
2020-04-21 17:33:01 +0300

    Created a subclass of UICollectionViewCell to show the collection of Followers on the FollowerListVC and created a custom UIImageView to hold the avatar (which for now just holds the placeholder)

---
+++ GHFollowers/Custom Subclasses/FollwerCell.swift
@@ -0,0 +1,55 @@
+//
+//  FollwerCell.swift
+//  GHFollowers
+//
+//  Created by Jacques Benzakein on 4/21/20.
+//  Copyright © 2020 Q Technologies. All rights reserved.
+//
+
+import UIKit
+
+class FollwerCell: UICollectionViewCell {
+    
+    static let reuseID = "FollowerCell"
+    let padding: CGFloat = 8
+    
+    let avatarImageView = GFAvatarImageView(frame: .zero)
+    let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureAvatarImageView()
+        configureAvatarImageView()
+    }
+    
+    func set(follower: Follower) {
+        usernameLabel.text = follower.login
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureAvatarImageView() {
+        addSubview(avatarImageView)
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            avatarImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor)
+        ])
+    }
+    
+    private func configureUsernameLabel() {
+        addSubview(usernameLabel)
+        
+        NSLayoutConstraint.activate([
+            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
+            usernameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            usernameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+}
