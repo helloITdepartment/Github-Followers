a7950c443fe42f0ec6ccb7fbe34cb88bffa9864a
Jacques Benzakein <Jackbenzakein@gmail.com>
2020-04-28 15:23:24 +0300

    Refactored the logic of getting the followers out of ViewDidLoad and into its own method. Created a Collection view and a method to configure it, and a method to create a flow layout for it that can adjust to different amounts of columns

--- GHFollowers/View Controllers/FollowerListVC.swift
+++ GHFollowers/View Controllers/FollowerListVC.swift
@@ -11,15 +11,37 @@ import UIKit
 class FollowerListVC: UIViewController {
     
     var username: String!
+    var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureViewController()
+        configureCollectionView()
+        getFollowers()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
 
+    private func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func configureCollectionView() {
+        
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createNColumnFlowLayout(withNumberOfColumns: 3)) //TODO:- if there's a settings page, let the user decide how many columns (within a certain range) and also change the number of followers loaded per page based on the same value
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollwerCell.self, forCellWithReuseIdentifier: FollwerCell.reuseID)
+        
+    }
+    
+    private func getFollowers() {
         
         NetworkManager.shared.getFollowers(for: username, page: 1) { result in
-            
             switch result {
             case .success(let followers):
                 print(followers.count)
@@ -28,10 +50,23 @@ class FollowerListVC: UIViewController {
                 self.presentGFAlertOnMainThread(title: "Uh oh.", message: error.rawValue, buttonTitle: "Got it.")
             }
         }
+        
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        navigationController?.setNavigationBarHidden(false, animated: true)
+    private func createNColumnFlowLayout(withNumberOfColumns n: Int) -> UICollectionViewFlowLayout {
+        
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let numberOfItemSpacings: CGFloat = CGFloat(n-1)
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * numberOfItemSpacings)
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+        
     }
-
 }
