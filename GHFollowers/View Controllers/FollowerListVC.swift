226845f3105f08b203ddaac46bc8e5bb4716e0b0
Jacques Benzakein <Jackbenzakein@gmail.com>
2020-08-03 19:20:32 +0300

    Implemented the functionality of the + button in the FollowerListVC Just pulls the info of the current user to get the avatarURL and then stores that follower objects in CoreData using the persistence manager

--- GHFollowers/View Controllers/FollowerListVC.swift
+++ GHFollowers/View Controllers/FollowerListVC.swift
@@ -144,7 +144,31 @@ class FollowerListVC: UIViewController {
     }
     
     @objc func addButtonTapped() {
-        print("Taptap")
+        self.showLoadingView()
+        
+        NetworkManager.shared.getUser(for: username) { [weak self] result in
+            guard let self = self else { return }
+            self.dismissLoadingView()
+            
+            switch result{
+            case .success(let user):
+                
+                let follower = Follower(login: user.login, avatarUrl: user.avatarUrl)
+                
+                PersistenceManager.updateFavorites(with: follower, actionType: .add) { [weak self] error in
+                    guard let self = self else { return }
+                    guard let error = error else { return }
+                    
+                    self.presentGFAlertOnMainThread(title: "Uh oh", message: error.rawValue, buttonTitle: "Ok")
+                }
+                
+            case .failure(let error):
+                
+                self.presentGFAlertOnMainThread(title: "Uh oh", message: error.rawValue, buttonTitle: "Ok")
+                
+            }
+        }
+        
     }
 }
 
