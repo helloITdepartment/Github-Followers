7a94fd72b3c86343755376d5fd0a5827eb20db9f
Jacques Benzakein <Jackbenzakein@gmail.com>
2020-08-05 18:36:38 +0300

    Added an extension on UIColor to return a random color

--- GHFollowers/Utilities/Extensions.swift
+++ GHFollowers/Utilities/Extensions.swift
@@ -96,3 +96,15 @@ extension Date {
     }
     
 }
+
+extension UIColor {
+    
+    static func random() -> UIColor {
+    
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+        return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
+    
+}
