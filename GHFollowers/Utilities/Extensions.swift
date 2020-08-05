f86d03c772512c5b460401949eb4a1dc90bfcea8
Jacques Benzakein <Jackbenzakein@gmail.com>
2020-08-05 19:07:43 +0300

    Gave the emptyStateView a background of systemBackground that way if it were to show up over, say, a tableview say, when the user has deleted the last favorite i the Favorite screen, you won't see what was behind it

--- GHFollowers/Utilities/Extensions.swift
+++ GHFollowers/Utilities/Extensions.swift
@@ -58,6 +58,7 @@ extension UIViewController {
     func showEmptyStateView(with message: String, in view: UIView) {
         let emptyStateView = GFEmptyStateView(message: message)
         emptyStateView.frame = view.bounds
+        emptyStateView.backgroundColor = .systemBackground
         view.addSubview(emptyStateView)
     }
     
